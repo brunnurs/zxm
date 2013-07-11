f7b5750ad8d2b5832d4dede7093ae54d688e38df
Stefan Schoeb <opendix@gmail.com>
2013-07-11 10:19:36 +0200

    Add layout for user-list-item; Add List of users to UserListVoewModel

--- Zxm.Core/ViewModels/Tabs/UserListViewModel.cs
+++ Zxm.Core/ViewModels/Tabs/UserListViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 using Cirrious.MvvmCross.ViewModels;
 
 namespace Zxm.Core.ViewModels.Tabs
 {
     public class UserListViewModel : MvxViewModel
     {
+        public UserListViewModel()
+        {
+            
+        }
+
+        private ObservableCollection<User> _users = new ObservableCollection<User>{new User{Name = "Hans"}, new User{Name = "Peter"}};
+        public ObservableCollection<User> Users
+        {
+            get { return _users; }
+            set
+            {
+                _users = value;
+                RaisePropertyChanged(() => Users);
+            }
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
     }
 }
