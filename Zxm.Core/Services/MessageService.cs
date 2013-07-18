5797b0a96cb006c0dd444046fec79fa182ec13f4
Stefan Schoeb <opendix@gmail.com>
2013-07-18 13:44:54 +0200

    Cleanup MessageService

--- Zxm.Core/Services/MessageService.cs
+++ Zxm.Core/Services/MessageService.cs
@@ -9,26 +9,15 @@ namespace Zxm.Core.Services
 {
     public class MessageService : IMessageService
     {
-        private Action<List<Message>> _messageCallback;
-
         // TODO: Duplicated in UserService
         private const string Url = "http://zxm.azurewebsites.net/";
 
 
         public void RequestMessages(Action<List<Message>> messageCallback)
         {
-            //TODO: Other way then instance variable
-            _messageCallback = messageCallback;
-
             var client = new RestClient(Url);
             var request = new RestRequest("message?format=json", Method.GET);
-            client.ExecuteAsync(request, RequestMessageCallback);
-        }
-
-        private void RequestMessageCallback(IRestResponse restResponse, RestRequestAsyncHandle arg2)
-        {
-            //TODO: RequestFormat?
-            _messageCallback(JsonConvert.DeserializeObject<List<Message>>(restResponse.Content));
+            client.ExecuteAsync(request, (response, x) => messageCallback(JsonConvert.DeserializeObject<List<Message>>(response.Content)));
         }
 
         public void SendMessage(Message newMessage)
@@ -37,11 +26,7 @@ namespace Zxm.Core.Services
             var request = new RestRequest("message?format=json", Method.POST);
             //TODO: use AddBody does not seem to work
             request.AddParameter("text/json", JsonConvert.SerializeObject(newMessage), ParameterType.RequestBody);
-            client.ExecuteAsync(request, SendMessageCallback);
-        }
-
-        private void SendMessageCallback(IRestResponse arg1, RestRequestAsyncHandle arg2)
-        {
+            client.ExecuteAsync(request, (arg1, arg2) => { });
         }
     }
 }
