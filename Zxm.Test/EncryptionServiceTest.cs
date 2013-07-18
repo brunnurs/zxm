3c7ef24507567444b862cd03d48043a597ae2685
obrack <oliver.brack@zuehlke.com>
2013-07-18 16:29:30 +0200

    Changed test project type

---
+++ Zxm.Test/EncryptionServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Zxm.Test
+{
+    [TestFixture]
+    public class EncryptionServiceTest
+    {
+        [Test]
+        public void EncryptString()
+        {
+            const string message = "Hallo Stefan, hallo Ursin, hallo Oliver!";
+            Assert.IsNotNull(message);
+        }
+    }
+}
