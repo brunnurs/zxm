d814e8d4d50c4e251b2b40610169cf220537b9a5
obrack <oliver.brack@zuehlke.com>
2013-07-18 15:15:33 +0200

    Initial check-in of webservice

---
+++ Zxm.Webservice/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using ServiceStack.WebHost.Endpoints;
+using Zxm.Webservice.Persistence;
+using Zxm.Webservice.Services;
+
+namespace Zxm.Webservice
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public class HelloAppHost : AppHostBase
+        {
+            //Tell Service Stack the name of your application and where to find your web services
+            public HelloAppHost() : base("Hello Web Services", typeof(UserService).Assembly) { }
+
+            public override void Configure(Funq.Container container)
+            {
+                container.Register(new UserRepository());
+                container.Register(new MessageRepository());
+            }
+        }
+
+        //Initialize your application singleton
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new HelloAppHost().Init();
+        }
+    }
+}
