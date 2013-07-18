0b22f6c0abe02110154dd068486ef6d90ac34fe4
obrack <oliver.brack@zuehlke.com>
2013-07-18 17:03:36 +0200

    Removed not working encryption

--- Zxm.Test/EncryptionServiceTest.cs
+++ Zxm.Test/EncryptionServiceTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+
+using NUnit.Framework;
+
+using Zxm.Core.Services;
 
 namespace Zxm.Test
 {
@@ -10,6 +14,21 @@ namespace Zxm.Test
         {
             const string message = "Hallo Stefan, hallo Ursin, hallo Oliver!";
             Assert.IsNotNull(message);
+
+            /*
+            var encryptionService = new EncryptionService();
+            var random = new Random();
+            var key = new byte[1024];
+            var iv = new byte[1024];
+            random.NextBytes(key);
+            random.NextBytes(iv);
+
+            var encryptedText = encryptionService.Encrypt(message, key, iv);
+            Assert.IsNotNull(encryptedText);
+            var plainText = encryptionService.Decrypt(encryptedText, key, iv);
+
+            Assert.AreEqual(message, plainText);
+             */
         }
     }
 }
