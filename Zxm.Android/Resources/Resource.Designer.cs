c915be1d09ce6810731eb67854094771cad82d82
Stefan Schoeb <opendix@gmail.com>
2013-07-20 18:22:49 +0200

    Add zuehlke drawables

--- Zxm.Android/Resources/Resource.Designer.cs
+++ Zxm.Android/Resources/Resource.Designer.cs
@@ -66,7 +66,7 @@ namespace Zxm.Android
 			public const int Add = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
 			public const int Refresh = 2130837506;
