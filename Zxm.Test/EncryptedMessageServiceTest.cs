181f79656e28add25e97ae245679d34e628770d4
Ursin Brunner <ursin.brunner@gmail.com>
2013-09-16 11:47:23 +0200

    still on test for conference

--- Zxm.Test/EncryptedMessageServiceTest.cs
+++ Zxm.Test/EncryptedMessageServiceTest.cs
@@ -2,15 +2,30 @@ using System;
 using NUnit.Framework;
 using NSubstitute;
 using Zxm.Core.Services;
+using Zxm.Core.Model;
 
 namespace Zxm.Test
 {
     [TestFixture]
     public class EncryptedMessageServiceTest
     {
+        const string EncryptedMessageString = "XXXXi'mEncryptedXXXX";
+
+        EncrytedMessageService testee;
+
+        EncryptionService encryptionServiceMock;
+        MessageService messageServiceMock;
+        UserSettingsService userSettingsServiceMock;
+
+
         [TestFixtureSetUp] 
         public void InitMocks()
-        { /* ... */ 
+        {
+            encryptionServiceMock = Substitute.For<EncryptionService>();
+            messageServiceMock = Substitute.For<MessageService>();
+            userSettingsServiceMock = Substitute.For<UserSettingsService>();
+
+            testee = new EncrytedMessageService(encryptionServiceMock, userSettingsServiceMock,messageServiceMock);
         }
 
 
@@ -18,12 +33,24 @@ namespace Zxm.Test
         public void MessageEncrypition()
         {
             /* when */
-            var encryptionServiceMock = Substitute.For<EncryptionService>();
-            var messageServiceMock = Substitute.For<IMessageService>();
+            encryptionServiceMock.Encrypt(Arg.Any<String>(), Arg.Any<Byte[]>()).Returns(EncryptedMessageString);
+
+            messageServiceMock.SendMessage(Arg.Is<Message>(m => m.Content == EncryptedMessageString), Arg.Invoke(new Message(), true));
+//            messageServiceMock
+//                .When(x=>x.SendMessage(Arg.Is<Message>(m => m.Content == EncryptedMessageString),Arg.Any<Action<Message,bool>>()))
+//                    .Do(x=>{
+//                        challengeAttemptRESTStoreMock.OnNetworkingError += Raise.Event<Action<Exception>>(new Exception("communication failed"));
+//                    });
+
+            var testmessage = new Message(){ Content = "This is the unencrypted text" };
+
+            bool result = false;
 
             /* given */
+            testee.SendMessage(testmessage, (Message m, bool wasSuccessfull) => result = wasSuccessfull);
 
             /* then */
+            Assert.IsTrue(result);
         }
     }
 }
