700780bb2bc96bec7c715f8d9abd4ea45bd06268
Stefan Schoeb <opendix@gmail.com>
2013-09-16 11:05:22 +0200

    Split message service into MessageService and EncryptedMessageService

---
+++ Zxm.Core/Services/EncrytedMessageService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Zxm.Core.Common;
+using Zxm.Core.Model;
+
+namespace Zxm.Core.Services
+{
+    public class EncrytedMessageService
+    {
+        private readonly EncryptionService _encryptionService;
+        private readonly UserSettingsService _userSettingsService;
+        private readonly MessageService _messageService;
+
+        public EncrytedMessageService(EncryptionService encryptionService,
+                                      UserSettingsService userSettingsService,
+                                      MessageService messageService)
+        {
+            _encryptionService = encryptionService;
+            _userSettingsService = userSettingsService;
+            _messageService = messageService;
+        }
+
+        public event EventHandler<EventArgs<Message>> MessageSent;
+
+        public void RequestMessages(Action<List<Message>> messageCallback)
+        {
+            _messageService.RequestMessages(list => RequestMessagesCallback(list, messageCallback));
+        }
+
+        public void SendMessage(Message newMessage, Action<Message, bool> messageSentCallback)
+        {
+            // Make a copy which can be used for the MessageSent-Event
+            var originalMessage = new Message(newMessage);
+
+            EncryptMessage(newMessage);
+
+            _messageService.SendMessage(newMessage, (message, successfully) => MessageSentCallback(originalMessage, successfully, messageSentCallback));
+        }
+
+        private void EncryptMessage(Message newMessage)
+        {
+            var encryptedContent = _encryptionService.Encrypt(newMessage.Content, GetKey());
+            newMessage.Content = encryptedContent;
+        }
+
+        private void RequestMessagesCallback(List<Message> list, Action<List<Message>> messageCallback)
+        {
+            list.ForEach(DecryptMessage);
+            messageCallback(list);
+        }
+
+        private byte[] GetKey()
+        {
+            return EncryptionService.GetKeyFromPassword(_userSettingsService.UserSettings.Password);
+        }
+
+        private void DecryptMessage(Message message)
+        {
+            try
+            {
+                byte[] key = GetKey();
+                var decryptedContent = _encryptionService.Decrypt(message.Content, key);
+                message.Content = decryptedContent;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("failed to decrypt message from {0}. Exception: {1}", message.Sender, ex);
+            }
+        }
+
+        private void MessageSentCallback(Message message, bool successfully, Action<Message, bool> messageSentCallback)
+        {
+            if (successfully)
+            {
+                if (MessageSent != null)
+                {
+                    MessageSent(this, new EventArgs<Message>(message));
+                }
+            }
+            messageSentCallback(message, successfully);
+        }
+    }
+}
--- Zxm.Core/Services/MessageService.cs
+++ Zxm.Core/Services/MessageService.cs
@@ -8,64 +8,40 @@ using System.Diagnostics;
 
 namespace Zxm.Core.Services
 {
-    public class MessageService : IMessageService
+    public class MessageService
     {
         private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
         {
             DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
         };
 
-        private readonly EncryptionService _encryptionService;
-        private readonly UserSettingsService _userSettingsService;
-
         public event EventHandler<EventArgs<Message>> MessageSent;
-
-        public MessageService(EncryptionService encryptionService, UserSettingsService userSettingsService)
-        {
-            _encryptionService = encryptionService;
-            _userSettingsService = userSettingsService;
-        }
 
         public void RequestMessages(Action<List<Message>> messageCallback)
         {
             Debug.WriteLine("RequestMessages called");
             var client = new RestClient(Config.WebserviceUrlApi);
             var request = new RestRequest("message?format=json", Method.GET);
-            client.ExecuteAsync(request, (response, x) => MessagesLoaded(response, messageCallback));
+            client.ExecuteAsync(request, (response, x) => MessagesLoadedCallback(response, messageCallback));
         }
 
-        public void SendMessage(Message newMessage, Action<Message,bool> messageSentCallback)
+        public void SendMessage(Message message, Action<Message, bool> messageSentCallback)
         {
-            // Make a copy which can be used for the MessageSent-Event
-            var originalMessage = new Message(newMessage);
-
-            // Encrypt message
-            var encryptedContent = _encryptionService.Encrypt(newMessage.Content, GetKey());
-            newMessage.Content = encryptedContent;
-
             var client = new RestClient(Config.WebserviceUrlApi);
             var request = new RestRequest("message?format=json", Method.POST);
             //TODO: use AddBody does not seem to work
-            string json = JsonConvert.SerializeObject(newMessage, SerializerSettings);
+            string json = JsonConvert.SerializeObject(message, SerializerSettings);
             request.AddParameter("text/json", json, ParameterType.RequestBody);
 
-            client.ExecuteAsync(request, (response, x) => MessageSentCallback(response, messageSentCallback, originalMessage));
+            client.ExecuteAsync(request, (response, x) => MessageSentCallback(response, messageSentCallback, message));
             Debug.WriteLine("sending new message...");
         }
 
-        private void MessagesLoaded(IRestResponse response, Action<List<Message>> messageCallback)
+        private void MessagesLoadedCallback(IRestResponse response, Action<List<Message>> messageCallback)
         {
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                var receivedMessages = JsonConvert.DeserializeObject<List<Message>>(response.Content, new JsonSerializerSettings
-                {
-                    DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
-                });
-
-                Debug.WriteLine ("received {0} new messages. Try to deserialize",receivedMessages.Count);
-                receivedMessages.ForEach(DecryptMessage);
-
-                Debug.WriteLine("deserializing worked");
+                var receivedMessages = JsonConvert.DeserializeObject<List<Message>>(response.Content, SerializerSettings);
                 messageCallback(receivedMessages);
             }
             else
@@ -74,23 +50,7 @@ namespace Zxm.Core.Services
             }
         }
 
-        private void DecryptMessage(Message message)
-        {
-            try
-            {
-                byte[] key = GetKey();
-                var decryptedContent = _encryptionService.Decrypt(message.Content, key);
-                message.Content = decryptedContent;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("failed to decrypt message from {0}. Exception: {1}", message.Sender, ex);
-            }
-        }
-
-
-
-        private void MessageSentCallback(IRestResponse response, Action<Message,bool> messageSentCallback, Message message)
+        private void MessageSentCallback(IRestResponse response, Action<Message, bool> messageSentCallback, Message message)
         {
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
@@ -99,18 +59,13 @@ namespace Zxm.Core.Services
                     MessageSent(this, new EventArgs<Message>(message));
                 }
                 Debug.WriteLine("message successfully sent");
-                messageSentCallback(message,true);
+                messageSentCallback(message, true);
             }
             else
             {
                 Debug.WriteLine("sending message failed {0}", response.StatusCode);
-                messageSentCallback(message,false);
+                messageSentCallback(message, false);
             }
-        }
-
-        private byte[] GetKey()
-        {
-            return EncryptionService.GetKeyFromPassword(_userSettingsService.UserSettings.Password);
         }
     }
 }
